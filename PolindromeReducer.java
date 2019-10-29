060ada62ec26ebd02d4cf6d54ea35d8663293264
karipkaya <57115248+karipkaya@users.noreply.github.com>
2019-10-29 14:55:43 +0300

    Create PolindromeReducer.java

---
+++ PolindromeReducer.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class PolindromeReducer extends MapReduceBase implements Reducer<IntWritable,Text, IntWritable,Text>{
+
+	@Override
+	public void reduce(IntWritable key, Iterator<Text> values, OutputCollector<IntWritable, Text> output,
+			Reporter reporter) throws IOException {
+		// put all words to output
+		while(values.hasNext())
+		output.collect(key, values.next());
+		
+	}
+
+
+}
